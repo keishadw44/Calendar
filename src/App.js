584e18586263586350dc38810ad5c0be5d3be4a5
Keisha W <keishaw@Keishas-Air.lan>
2023-02-02 17:09:49 -0500

    First push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Year, Agenda, RecurrenceEditor, ViewsDirective, ViewDirective, TimelineViews, TimelineMonth } from '@syncfusion/ej2-react-schedule'
+
 function App() {
+const EventSetting = [
+{
+  Id: 1,
+  Subject: 'Test',
+  StartTime: new Date(2023, 1, 16, 10, 0),
+  EndTime: new Date(2023, 1, 16, 12, 30),
+  IsAllDay: true,
+  Status: 'Completed',
+  Priority: 'High',
+  Recurrence: 'Freq=Weekly; Interval=1; Count=10',
+  IsReadonly: true,
+  isBlock: true,
+},
+// {
+//   Id: 2, 
+//   Subject: 'Meeting',
+//   StartTime: new Date(2023, 1, 15, 10, 0),
+//   EndTime: new Date(2023, 1, 15, 12, 30),
+//   IsAllDay: false,
+//   Status: 'Completed',
+//   Priority: 'High'
+// }
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScheduleComponent currentView='Month' newSetting={{dataSource: EventSetting}} selectDate={new Date(2023, 1, 1)}>
+      <ViewsDirective>
+        <ViewDirective option='Week'></ViewDirective>
+        <ViewDirective option='Day'></ViewDirective>
+        <ViewDirective option='Month'></ViewDirective>
+        <ViewDirective option='Year'></ViewDirective>
+        <ViewDirective option='TimelineDay'></ViewDirective>
+        <ViewDirective option='TimelineMonth'></ViewDirective>
+        
+      </ViewsDirective>
+     <Inject services={[Day, Week, WorkWeek, Month, Year, Agenda, TimelineViews, TimelineMonth ]} />
+    </ScheduleComponent>
   );
 }
 
